51dc668943f03ddd6c18c00b742216567a4ba89a
yashamarquise <yashamarquise@gmail.com>
2023-06-18 23:14:23 +0300

    done

---
+++ app/src/main/java/com/yasha/task2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.yasha.task2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.buttonForward);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view) {
+                EditText question = findViewById(R.id.questionText);
+                Intent intent = new Intent(view.getContext(), SecondActivity.class);
+                intent.putExtra("question", question.getText().toString());
+                startActivity(intent);
+            }
+        });
+        Bundle extra = getIntent().getExtras();
+        if(extra!=null){
+            String value = extra.getString("answer");
+            TextView textView = (TextView) findViewById(R.id.answerText);
+            textView.setText(value);
+        }
+    }
+}
---
+++ app/src/main/java/com/yasha/task2/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.yasha.task2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button button = (Button) findViewById(R.id.buttonBack);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view) {
+                EditText answer = findViewById(R.id.answer);
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                intent.putExtra("answer", answer.getText().toString());
+                startActivity(intent);
+            }
+        });
+        Bundle extra = getIntent().getExtras();
+        if(extra != null){
+            String value = extra.getString("question");
+            TextView textView = (TextView) findViewById(R.id.question);
+            textView.setText(value);
+        }
+    }
+}
